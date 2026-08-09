788a5af2bcbbdfa458b35bb7cff06670af25e785
Christopher Di Bella <cjdb@google.com>
2026-08-09 02:14:09 +0000

    Attempt to get SelfDestruct working

--- toolchain/check/control_flow.cpp
+++ toolchain/check/control_flow.cpp
@@ -157,7 +157,7 @@ static auto AddCleanups(Context& context, ScopeStack::CleanupScopeDepth depth)
     // cleanup blocks, so we'll want to avoid this in the future.
     BuildUnaryOperator(
         context, context.insts().GetLocIdForDesugaring(destroy_id),
-        {.interface_name = CoreIdentifier::SubobjectDestroy}, destroy_id);
+        {.interface_name = CoreIdentifier::SelfDestructible}, destroy_id);
   }
 }
 
